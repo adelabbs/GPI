128d798460c48ab90f9cbcf0c4e9177b15bada79
geoff <geoff@DESKTOP-AU83QBR>
2021-01-15 15:38:36 +0100

    # WARNING: head commit changed in the meantime
    Geoffroy (Chef) Classe test

---
+++ src/test/GeoffroyClass.java
@@ -0,0 +1,16 @@
+package test;
+/*
+ * Classe test de l'activité 1
+ * 
+ */
+public class GeoffroyClass {
+	String nom;
+	String prenom;
+	int age;
+	
+	public GeoffroyClass(String nom, String prenom, int age) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+}
