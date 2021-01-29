41227367f7ad95444cc3ff2c17154a14b8de960e
Azilisiev <bordeneuve.awen@gmail.com>
2021-01-29 16:13:18 +0100

    removed type from insect manager

--- src/prototype/process/managers/BugManager.java
+++ src/prototype/process/managers/BugManager.java
@@ -5,23 +5,13 @@ import prototype.data.Insect;
 
 public abstract class BugManager {
 
-	private String type = "none";
 	private String groupID = "0";
 	private String agressivity = "peaceful";
 
-	public BugManager(String type, String groupID, String agressivity) {
+	public BugManager( String groupID, String agressivity) {
 
-		this.type = type;
 		this.groupID = groupID;
 		this.agressivity = agressivity;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
 	}
 
 	public String getGroupID() {
