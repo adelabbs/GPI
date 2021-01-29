839e0ee3e532e2f86fae0cf80bdd8a71c57f0b8f
Amine Sabbahi <amine.sabbahi@gmail.com>
2021-01-29 15:52:06 +0100

    add environment

--- src/prototype/data/Environment.java
+++ src/prototype/data/Environment.java
@@ -2,4 +2,21 @@ package prototype.data;
 
 public class Environment {
 	
+	private int line;
+	private int column;
+	private int [][] map;
+	
+	public Environment(int line, int column) {
+		this.line= line;
+		this.column=column;
+		map = new int[line][column];
+		
+		for(int i=0; i< this.line; i++) {
+			for(int j=0; j< this.column; j++) {
+				this.map[i][j]= 0;
+			}
+		}
+		
+	}
+	
 }
