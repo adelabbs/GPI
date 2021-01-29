82c15c23e82175ebc1fed42e41fc7dc647b98c26
Azilisiev <bordeneuve.awen@gmail.com>
2021-01-29 14:35:58 +0100

    Added BugManager

---
+++ src/prototype/process/managers/BugManager.java
@@ -0,0 +1,26 @@
+package prototype.process.managers;
+
+public class BugManager {
+	private String type = "none";
+	private String groupID = "0";
+	private String agressivity = "peaceful";
+	public BugManager() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getGroupID() {
+		return groupID;
+	}
+
+	public void setGroupID(String groupID) {
+		this.groupID = groupID;
+	}
+
+}
