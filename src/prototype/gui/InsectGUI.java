9664e18af235210f5437bbee663786414f885eac
geoff <geoff@DESKTOP-AU83QBR>
2021-01-29 15:36:40 +0100

    Update GUI (run) and updateValues

--- src/prototype/gui/InsectGUI.java
+++ src/prototype/gui/InsectGUI.java
@@ -7,8 +7,11 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.sun.source.tree.TreeVisitor;
+
 import prototype.process.Simulation;
 import prototype.process.SimulationEntry;
+import prototype.process.managers.BugManager;
 
 /*
  * Main GUI class for simulation
@@ -18,11 +21,14 @@ public class InsectGUI extends JFrame implements Runnable {
 	private static final Dimension IDEAL_MAIN_DIMENSION = new Dimension(800, 400);
 	private static final Dimension IDEAL_DASHBOARD_DIMENSION = new Dimension(800, 300);
 	
+	private static final int TICK = 500;
+	
 		
 	private Dashboard dashboard = new Dashboard();
 	
 	private SimulationEntry simEntry = new SimulationEntry();
 	private Simulation simulation = new Simulation(simEntry);
+		
 	
 	private boolean stop = true;
 	
@@ -47,6 +53,7 @@ public class InsectGUI extends JFrame implements Runnable {
 	}
 	
 	public void updateValues() {
+		
 		dashboard.setPosX(DISPOSE_ON_CLOSE);
 		dashboard.setPosY(DISPOSE_ON_CLOSE);
 		
@@ -54,11 +61,16 @@ public class InsectGUI extends JFrame implements Runnable {
 		dashboard.repaint();
 	}
 	
-	
+
 	@Override
 	public void run() {
 		while(!stop) {
-			
+			try {
+				Thread.sleep(TICK);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+			simulation.simulate();
 			if(!stop) {
 				updateValues();
 			}
