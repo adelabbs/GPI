7f299dcbee70806cc3b52037f9d17dc7760fd9cc
Adel <57968261+dedely@users.noreply.github.com>
2021-01-29 15:19:46 +0100

    update insect model

--- src/prototype/data/Ant.java
+++ src/prototype/data/Ant.java
@@ -2,9 +2,9 @@ package prototype.data;
 
 import prototype.process.InsectVisitor;
 
-public class Bee extends Insect {
+public class Ant extends Insect {
 
-	public Bee(Integer id, Coordinate coordinates, int maxHealth, int maxHunger, int maxThirst, int speed) {
+	public Ant(Integer id, Coordinate coordinates, int maxHealth, int maxHunger, int maxThirst, int speed) {
 		super(id, coordinates, maxHealth, maxHunger, maxThirst, speed);
 	}
 
@@ -12,6 +12,5 @@ public class Bee extends Insect {
 	<T> T accept(InsectVisitor<T> visitor) {
 		return visitor.visit(this);
 	}
-	
 	
 }
--- src/prototype/data/Insect.java
+++ src/prototype/data/Insect.java
@@ -1,24 +1,124 @@
 package prototype.data;
 
+import prototype.process.InsectVisitor;
+
+/**
+ * The Insect model
+ *
+ */
 public abstract class Insect {
-	
-	private int life;
-	private int food;
-	private int water;
-	private int attack;
-	
-	public Insect(int id){
-		this.life=10;
-		this.food=10;
-		this.water=10;
-		this.attack=2;
+
+	private Integer id;
+	private Coordinate coordinates;
+
+	private int maxHealth;
+	private int currentHealth;
+	private int currentHunger;
+	private int maxHunger;
+	private int currentThirst;
+	private int maxThirst;
+	private int speed;
+	private int maxSpeed;
+	private int currentSpeed;
+
+	public Insect(Integer id, Coordinate coordinates, int maxHealth, int maxHunger, int maxThirst, int speed) {
+		this.id = id;
+		this.coordinates = coordinates;
+		this.maxHealth = maxHealth;
+		currentHealth = maxHealth;
+		this.maxHunger = maxHunger;
+		currentHunger = maxHunger;
+		this.maxThirst = maxThirst;
+		currentThirst = maxThirst;
+		this.maxSpeed = speed;
+		currentSpeed = maxSpeed;
 	}
 	
-	public abstract void eat();
-	
-	public abstract void drink();
-	
-	public abstract void death();
-	
-	public abstract void attack();
+	abstract <T>  T accept(InsectVisitor<T> visitor);
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public Coordinate getCoordinates() {
+		return coordinates;
+	}
+
+	public void setCoordinates(Coordinate coordinates) {
+		this.coordinates = coordinates;
+	}
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+
+	public int getCurrentHunger() {
+		return currentHunger;
+	}
+
+	public void setCurrentHunger(int currentHunger) {
+		this.currentHunger = currentHunger;
+	}
+
+	public int getMaxHunger() {
+		return maxHunger;
+	}
+
+	public void setMaxHunger(int maxHunger) {
+		this.maxHunger = maxHunger;
+	}
+
+	public int getCurrentThirst() {
+		return currentThirst;
+	}
+
+	public void setCurrentThirst(int currentThirst) {
+		this.currentThirst = currentThirst;
+	}
+
+	public int getMaxThirst() {
+		return maxThirst;
+	}
+
+	public void setMaxThirst(int maxThirst) {
+		this.maxThirst = maxThirst;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public int getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public void setCurrentSpeed(int currentSpeed) {
+		this.currentSpeed = currentSpeed;
+	}
+
 }
