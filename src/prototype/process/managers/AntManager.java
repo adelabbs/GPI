657c763c36b2a23919b662db2e9253ba1f4bb047
Azilisiev <bordeneuve.awen@gmail.com>
2021-02-05 16:54:37 +0100

    implemented some methods in AntManager

--- src/prototype/process/managers/AntManager.java
+++ src/prototype/process/managers/AntManager.java
@@ -7,6 +7,8 @@ import prototype.data.Insect;
 public class AntManager extends BugManager {
 
 	private Ant insect;
+	private boolean thirsty;
+	private boolean hungry;
 
 	public AntManager(String groupID, String agressivity, Ant insect) {
 		super(groupID, agressivity);
@@ -15,10 +17,73 @@ public class AntManager extends BugManager {
 
 	@Override
 	public void update() {
+
+		if ((insect.getCurrentThirst() / insect.getMaxThirst()) <= 0.30) {
+			this.goDrink();
+			thirsty = true;
+		}
+
+		else if ((insect.getCurrentHunger() / insect.getMaxHunger()) <= 0.30) {
+			this.goEat();
+			hungry = true;
+		}
+
 		if (insect.getDestinationPosition() == insect.getCurrentPosition()) {
-			insect.setDestinationPosition(new Coordinate(Math.random()*100, Math.random()*100));
+			if (thirsty) {
+				drink();
+
+			}
+			if (hungry) {
+				eat();
+			}
+
+			insect.setDestinationPosition(new Coordinate(Math.random() * 100, Math.random() * 100));
 		}
 		super.moveInsect(insect);
+
+	}
+
+	public void eat() {
+		//eat qui rempli totalement la faim de l'insecte
+		int currentHunger = insect.getCurrentHunger();
+		int maxHunger = insect.getMaxHunger();
+		int delta = maxHunger - currentHunger;
+		insect.setCurrentHunger(insect.getMaxHunger());
+		// TODO décrémenter la ressource de delta
+
+	}
+	
+	public void eat(int quantity) {
+		//eat qui rempli de quantity la faim de l'insecte
+		int currentHunger = insect.getCurrentHunger();
+		int maxHunger = insect.getMaxHunger();
+		int calculatedHunger = currentHunger + quantity;
+		
+		if(calculatedHunger>maxHunger) {			
+			insect.setCurrentHunger(insect.getMaxHunger());
+			quantity=calculatedHunger-maxHunger;
+		}
+		else {
+			insect.setCurrentHunger(currentHunger + quantity);
+		}
+		
+		// TODO décrémenter la ressource de quantity
+
+	}
+
+	private void drink() {
+		insect.setCurrentThirst(insect.getMaxThirst());
+		// TODO décrémenter la ressource
+
+	}
+
+	private void goEat() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void goDrink() {
+		// TODO Auto-generated method stub
 
 	}
 
