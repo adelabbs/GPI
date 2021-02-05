ad490a3c305e792ba7a6ab3007bed7de61171ac4
lespi <lespi@DESKTOP-4PPDA5P>
2021-02-05 16:36:51 +0100

    Natural resource with position and id

--- src/prototype/data/NaturalResource.java
+++ src/prototype/data/NaturalResource.java
@@ -2,11 +2,42 @@ package prototype.data;
 
 public class NaturalResource {
 	private String type;
+	private String id;
 	private int quantity;
+	private float xPos;
+	private float yPos;
 
-	public NaturalResource (String type, int quantity) {
+
+	public NaturalResource (String type, String id, int quantity, float x, float y) {
 		this.type = type;
+		this.id = id;
 		this.quantity = quantity;
+		this.xPos = x;
+		this.yPos = y;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public float getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(float xPos) {
+		this.xPos = xPos;
+	}
+
+	public float getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(float yPos) {
+		this.yPos = yPos;
 	}
 
 	public String getType() {
