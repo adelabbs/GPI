dce0fb42a21e3a61b33e05bc2898992c209f1f67
Azilisiev <bordeneuve.awen@gmail.com>
2021-02-12 15:50:23 +0100

    TileCoordinate class

---
+++ src/data/TileCoordiante.java
@@ -0,0 +1,32 @@
+package data;
+
+public class TileCoordiante {
+	private int abscissa;
+	private int ordinate;
+
+	public TileCoordiante(int abscissa, int ordinate) {
+		super();
+		this.abscissa = abscissa;
+		this.ordinate = ordinate;
+	}
+
+	public int getAbscissa() {
+		return abscissa;
+	}
+
+	public void setAbscissa(int abscissa) {
+		this.abscissa = abscissa;
+	}
+
+	public int getOrdinate() {
+		return ordinate;
+	}
+
+	public void setOrdinate(int ordinate) {
+		this.ordinate = ordinate;
+	}
+	
+	
+
+
+}
