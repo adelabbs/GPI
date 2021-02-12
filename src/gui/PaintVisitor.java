f497d4f54cc8996ee98ea7eeee10f7eb9674d766
geoff <geoff@DESKTOP-AU83QBR>
2021-02-12 14:08:20 +0100

    rotate image with angle

--- src/gui/PaintVisitor.java
+++ src/gui/PaintVisitor.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -25,6 +26,7 @@ public class PaintVisitor implements InsectVisitor<Void> {
 			BufferedImage bufferImage = ImageIO.read(new File(filename));
 			g.drawImage(bufferImage, (int) insect.getCurrentPosition().getAbscissa(),
 					(int) insect.getCurrentPosition().getOrdinate(), null);
+			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -37,7 +39,8 @@ public class PaintVisitor implements InsectVisitor<Void> {
 		String filename = "resources/images/fourmi_test2.png";
 		try {
 			BufferedImage bufferImage = ImageIO.read(new File(filename));
-			flip(bufferImage);
+			//flip(bufferImage);
+			bufferImage = rotate(bufferImage, 50);
 			g.drawImage(bufferImage, (int) insect.getCurrentPosition().getAbscissa(),
 					(int) insect.getCurrentPosition().getOrdinate(), null);
 
@@ -55,5 +58,17 @@ public class PaintVisitor implements InsectVisitor<Void> {
 				image.setRGB(i, image.getHeight() - j - 1, tmp);
 			}
 	}
+	
+	public static BufferedImage rotate(BufferedImage bimg, double angle) {
+		// width and height equals to height to avoid image crop
+	    int w = bimg.getHeight();    
+	    int h = bimg.getHeight();
 
+	    BufferedImage rotated = new BufferedImage(w, h, bimg.getType());  
+	    Graphics2D graphic = rotated.createGraphics();
+	    graphic.rotate(Math.toRadians(angle), w/2, h/2);
+	    graphic.drawImage(bimg, null, 0, 0);
+	    graphic.dispose();
+	    return rotated;
+	}
 }
