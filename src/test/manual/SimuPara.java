643595a5c79d79e8f792c17537d9b693d021bf69
Adel <57968261+dedely@users.noreply.github.com>
2021-02-12 15:03:24 +0100

    update simuPara

--- src/test/manual/SimuPara.java
+++ src/test/manual/SimuPara.java
@@ -2,10 +2,10 @@ package test.manual;
 
 public class SimuPara {
 	public static final int SIMULATION_SPEED = 20;
-
 	public static final int SIMULATION_MAP_SIZE = 1000;
+	public static final int SCALE = 50;
+	public static final int SIMULATION_TILES = SIMULATION_MAP_SIZE/SCALE;
 	public static final int SIMULATION_INSECT_COUNT_PER_TYPE = 2;
 
-	public static final int SCALE = 50;
 
 }
