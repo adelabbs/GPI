8dc3b514aae3999f21ab31f675f07c8bb90366ad
geoff <geoff@DESKTOP-AU83QBR>
2021-03-05 16:49:56 +0100

    add spider random movement and death

--- src/process/manager/SpiderManager.java
+++ src/process/manager/SpiderManager.java
@@ -1,28 +1,35 @@
 package process.manager;
 
+import data.Coordinate;
 import data.Environment;
 import data.Insect;
 import data.Spider;
+import test.manual.SimuPara;
 
 public class SpiderManager extends BugManager {
 
-	private Spider spider;
+	private Spider insect;
 
 	public SpiderManager(String groupID, String agressivity, Environment environment, Spider spider) {
 		super(groupID, agressivity, environment);
-		this.spider = spider;
+		this.insect = spider;
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		updateStats();
+		discoverPOI();
+		if (insect.getDestinationPosition() == insect.getCurrentPosition()) {
+			insect.setDestinationPosition(new Coordinate(Math.random() * SimuPara.SIMULATION_MAP_SIZE,
+					Math.random() * SimuPara.SIMULATION_MAP_SIZE));
+		}
+		super.moveInsect(insect);
 
 	}
 
 	@Override
 	public Insect getInsect() {
-		return spider;
-
+		return insect;
 	}
 
 }
