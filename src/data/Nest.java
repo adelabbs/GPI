a9158bff1de7248e68f22c789d6596dc9ed93391
Amine Sabbahi <amine.sabbahi@gmail.com>
2021-03-26 15:24:38 +0100

    added nest

--- src/data/Nest.java
+++ src/data/Nest.java
@@ -12,7 +12,7 @@ public class Nest {
 	private Integer id;
 	private String type;
 
-	private Coordinate Position;
+	private Coordinate position;
 	private ArrayList<Insect> insects = new ArrayList<Insect>();
 
 	private int size;
@@ -24,8 +24,92 @@ public class Nest {
 	private int currentHealth;
 	private int lifeSpan;
 	
-	public Nest(Integer id, Coordinate destinationPosition, int maxHealth, int maxHunger, int maxThirst, int maxSpeed,
-			String type) {
-		
+	public Nest(Integer id, String type, Coordinate position, int maxHealth,
+			int capacity) {
+		this.id=id;
+		this.position=position;
+		this.maxHealth=maxHealth;
+		this.type=type;
+		lifeSpan = (int) (MIN_LIFESPAN + Math.random() * ((MAX_LIFESPAN - MIN_LIFESPAN) + 1));
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Coordinate getPosition() {
+		return position;
+	}
+
+	public void setPosition(Coordinate position) {
+		this.position = position;
+	}
+
+	public ArrayList<Insect> getInsects() {
+		return insects;
+	}
+
+	public void setInsects(ArrayList<Insect> insects) {
+		this.insects = insects;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int getCurrentInsects() {
+		return currentInsects;
+	}
+
+	public void setCurrentInsects(int currentInsects) {
+		this.currentInsects = currentInsects;
+	}
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+
+	public int getLifeSpan() {
+		return lifeSpan;
+	}
+
+	public void setLifeSpan(int lifeSpan) {
+		this.lifeSpan = lifeSpan;
 	}
 }
